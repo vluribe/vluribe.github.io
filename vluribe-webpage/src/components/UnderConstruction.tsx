56ff31aa38653624b93d20c9eed13d7b41981fd2
Laura Uribe <lauravud@gmail.com>
2024-01-28 18:15:05 -0500

    Website first release

---
+++ vluribe-webpage/src/components/UnderConstruction.tsx
@@ -0,0 +1,23 @@
+import { Box, Center, Heading, Text } from "@chakra-ui/react";
+
+const UnderConstruction = () => {
+  return (
+    <Box
+      w="100vw"
+      h="100vh"
+      bgGradient="linear(red.100 0%, orange.100 25%, yellow.100 50%)"
+    >
+      <Center h="full">
+        <Box>
+          <Heading color="RGBA(0, 0, 0, 0.64)">This site is under construction</Heading>
+          <Text fontSize="md" color="RGBA(0, 0, 0, 0.64)">
+            This site is currently being developed, please come back later to
+            see more
+          </Text>
+        </Box>
+      </Center>
+    </Box>
+  );
+};
+
+export default UnderConstruction;
